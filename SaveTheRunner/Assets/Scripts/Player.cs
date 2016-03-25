146b1f656abff592c45f11cad2c96f72c4c72c58
sloizou <stathisloizou32@gmail.com>
2016-03-25 16:28:01 +0200

    Scene with Road and Sphere
    Created a scene with a road as prefab

---
+++ SaveTheRunner/Assets/Scripts/Player.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	private bool isJumping;
+	private bool isMovingLeft;
+	private bool isMovingRight;
+	private int i, j;
+
+	// Use this for initialization
+	void Start () {
+		isJumping = false;
+		isMovingLeft = false;
+		isMovingRight = false;
+		i = 0;
+		j = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.LeftArrow) && transform.position.x >= -0.5f) {
+			isMovingLeft = true;
+			//transform.Translate(new Vector3 (-1.0f, 0.0f, 0.0f));
+		}
+
+		if (Input.GetKeyDown (KeyCode.RightArrow) && transform.position.x <= 0.5f) {
+			isMovingRight = true;
+		}
+
+		if (Input.GetKeyDown (KeyCode.UpArrow) && !isJumping) {
+			GetComponent<Rigidbody> ().AddForce (new Vector3(0.0f, 1.0f, 0.0f) * 200.0f);
+			isJumping = true;
+		}
+
+		if (isMovingLeft) {
+			transform.Translate (new Vector3 (-0.05f, 0.0f, 0.0f));
+			if (i++ >= 19) {
+				isMovingLeft = false;
+				i = 0;
+			}
+		}
+
+		if (isMovingRight) {
+			transform.Translate (new Vector3 (0.05f, 0.0f, 0.0f));
+			if (j++ >= 19) {
+				isMovingRight = false;
+				j = 0;
+			}
+		}
+	}
+
+	void OnCollisionEnter(Collision other) {
+		if (other.gameObject.tag == "Ground") {
+			Debug.Log ("Collides with Ground");
+			isJumping = false;
+		}
+	}
+}
---
+++ SaveTheRunner/Assets/Scripts/RoadCreator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoadCreator : MonoBehaviour {
+	private int noRoads = 0;
+	public Transform roadPrefab;
+	private bool isCreating;
+
+	// Use this for initialization
+	void Start () {
+		isCreating = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject road = GameObject.FindGameObjectWithTag ("Ground");
+		if (road.transform.position.z < -15.0f && !isCreating) {
+			Instantiate (roadPrefab, new Vector3 (0, 0, road.transform.position.z + 50.0f - road.GetComponent<Road>().getSpeed()), Quaternion.identity);
+			isCreating = true;
+		}
+	}
+}
